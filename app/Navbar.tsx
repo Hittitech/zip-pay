aadfc037dab7f50aa0b717b5d7fa676c0ba1bad5
Hittitech <mdadnansami9381@gmail.com>
2023-12-07 11:00:46 +0530

    Build Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Logo from "../public/Logo.png"
+import Image from 'next/image'
+import OrnageButton from './components/OrnageButton'
 const Navbar = () => {
+  
   return (
-    <div>Navbar</div>
+    <div className='w-full pt-10 bg-black text-white'>
+    <nav className="flex justify-between w-3/4 m-auto space-x-6   px-5 h-14  items-center ">
+       <Link href={"/"}>
+        <Image alt='fampay-logo' width={148} height={43} src={Logo}></Image>
+        </Link>
+        <ul className='flex gap-x-12 items-center justify-between'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <OrnageButton
+          text="Download App"
+          />
+
+        </ul>
+    </nav>
+    </div>
   )
 }
 
